6d50facc8e007a2847e2d1b244ed74869a59425f
ishaksebsib <ishaksebsib@gmail.com>
2023-11-18 20:07:58 +0300

    setcurity update and removed unwanted list from main file

--- databaseConn.js
+++ databaseConn.js
@@ -2,15 +2,11 @@ const { log } = require("console");
 const { default: mongoose } = require("mongoose");
 
 require("dotenv").config();
-const uri =
-  "mongodb+srv://nebiyumusbah378:nebiyusadamishaq@cluster0.tlhkvat.mongodb.net/";
-
+const url = process.env.DATA_BASE_URL
 let client;
 
 async function connectToDatabase() {
   try {
-    const uri =
-      "mongodb+srv://nebiyumusbah378:nebiyusadamishaq@cluster0.tlhkvat.mongodb.net/";
     if (mongoose.connection.readyState != 1) {
       await mongoose.connect(uri);
       console.log("Connected to the database");
